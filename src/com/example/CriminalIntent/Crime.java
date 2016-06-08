115a8894dcd9063bf24c2b340a6c411bcbed3382
gaieepo <gaieepo@gmail.com>
2016-06-08 19:51:18 +0800

    add basic fragment layout

---
+++ src/com/example/CriminalIntent/Crime.java
@@ -0,0 +1,27 @@
+package com.example.CriminalIntent;
+
+import java.util.UUID;
+
+/**
+ * Created by gaieepo on 8/6/2016.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
--- src/com/example/CriminalIntent/CrimeActivity.java
+++ src/com/example/CriminalIntent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.example.CriminalIntent;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 public class CrimeActivity extends Activity {
@@ -11,5 +13,13 @@ public class CrimeActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction().add(R.id.fragmentContainer, fragment).commit();
+        }
     }
 }
